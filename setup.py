9f312fe09946e5ae447139e20197214c7e589d51
Oleg Bugrimov <obugrimov@avito.ru>
2021-11-16 11:14:35 +0300

    Version up

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ packages = ['aqueduct']
 setup(
     name='aqueduct',
     packages=find_packages(),
-    version='1.8.0',
+    version='1.8.1',
     license='MIT',
     license_files='LICENSE.txt',
     author='Data Science SWAT',
